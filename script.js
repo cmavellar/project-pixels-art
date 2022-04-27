401f63ea7709d48a1609c2ca8c9ba785b9966e06
Cássia Avellar <c.m.avellar@gmail.com>
2022-04-27 10:41:24 -0300

    Requisito 8 realizado; alterando estrutura das li no html

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
+// let N = 10;
+
+
+
 let colours = document.getElementsByClassName("color");
-// colours.addEventListener("click", selectColor)
 
 for (let index = 0; index < colours.length; index +=1) {
   colours[index].addEventListener("click", changeSelectedColor)
 } 
 
-function changeSelectedColor(event) {
-    // console.log("ANTES DE ATUALIZAR:");
-    // console.log(event.target.outerHTML);
-    // // console.log(event.target.classList);
-    let currentColorSelected = document.getElementsByClassName("selected")[0]     
-    currentColorSelected.classList.remove("selected");
+let pixels = document.getElementsByClassName("pixel");
 
-    event.target.classList.add("selected")
- 
-    // // console.log("DEPOIS DE ATUALIZAR:");
-    // console.log(event.target.outerHTML);
-    // console.log(event.target.classList);
-
-    // console.log(event);
- 
-    // console.log(currentColorSelected);
+for (let index = 0; index < pixels.length; index +=1) {
+  pixels[index].addEventListener("click", changeColor)
 }
+let currentColorSelected = document.getElementsByClassName("selected")[0];
+
+function changeSelectedColor(event) {
+
+    currentColorSelected.classList.remove("selected"); 
+    currentColorSelected = event.target;
+    currentColorSelected.classList.add("selected");  
+    
+}
+
+
+function changeColor (event) {
+
+  const cssObj = window.getComputedStyle(currentColorSelected, null);
+  let backgroundColor = cssObj.getPropertyValue("background-color");
+  console.log(backgroundColor);
+
+  event.target.style.backgroundColor = backgroundColor;   
+}
+
+
+
+
+
 
