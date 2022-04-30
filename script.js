970d508f6167fa5ddf39dd3741e1419b1071ac77
Cássia Avellar <c.m.avellar@gmail.com>
2022-04-29 22:59:48 -0300

    Acertando o Lint -2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function generateColor() { // Referência retirada do site, até linha 17, de 'H
   const letters = '0123456789ABCDEF';
   let color = '#';
 
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i+=1 ) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
@@ -16,45 +16,45 @@ cor2.style.backgroundColor = generateColor();
 cor3.style.backgroundColor = generateColor();
 cor4.style.backgroundColor = generateColor();
 
-const divList = document.getElementById('pixel-board'); //div que dará origem a outras ul/li
+const divList = document.getElementById('pixel-board'); // div que dará origem a outras ul/li
 
 const inputBoardSize = document.getElementById('board-size'); // input de criacao de mais pixels
 
 const buttonVQV = document.getElementById('generate-board'); // botao
-buttonVQV.addEventListener("click", createInputPixelSquare);
+buttonVQV.addEventListener('click', createInputPixelSquare);
 
 let N = 5;
 // createPixelSquare();
 
 // input do usuario para criar um quadrado de pixels personalizado
-function createInputPixelSquare() { 
-  const inputValue  = inputBoardSize.value;
+function createInputPixelSquare() {
+  const inputValue = inputBoardSize.value;
 
   if (inputValue == 0) {
     alert('Board inválido!');
-  } else { 
+  } else {
     N = inputValue;
-  if (inputValue < 5) {
-    N = 5;
-  }
+    if (inputValue < 5) {
+      N = 5;
+    }
     if (inputValue > 50) {
       N = 50;
-  }
+    }
     divList.innerHTML = '';
-    createPixelSquare ();
+    createPixelSquare();
   }
 }
- // cria o quadrado de pixels
+// cria o quadrado de pixels
 function createPixelSquare() {
   for (let index = 0; index < N; index += 1) {
     const lineUl = document.createElement('ul');
     divList.appendChild(lineUl);
     for (let index2 = 0; index2 < N; index2 += 1) {
-      const pixelLi = document.createElement('li')
+      const pixelLi = document.createElement('li');
       pixelLi.className = 'pixel';
       pixelLi.addEventListener('click', changeColor);
       lineUl.appendChild(pixelLi);
-      }
+    }
   }
 }
 
@@ -64,12 +64,12 @@ for (let index = 0; index < colours.length; index += 1) {
   colours[index].addEventListener('click', changeSelectedColor);
 }
 
-const pixels = document.getElementsByClassName("pixel");
+const pixels = document.getElementsByClassName('pixel');
 
 for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener("click", changeColor);
+  pixels[index].addEventListener('click', changeColor);
 }
-let currentColorSelected = document.getElementsByClassName("selected")[0];
+let currentColorSelected = document.getElementsByClassName('selected')[0];
 
 const button = document.getElementById('clear-board');
 button.addEventListener('click', clearPixels);
@@ -79,16 +79,16 @@ function changeSelectedColor(event) {
   currentColorSelected.classList.remove('selected');
   currentColorSelected = event.target;
   currentColorSelected.classList.add('selected');
-}  
+}
 // preenche os pixels de acordo com a cor da paleta selecionada
-function changeColor(event) { 
-  const cssObj = window.getComputedStyle(currentColorSelected, null); // Referência retirada, até linha 18, do site W3 Schools <https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp> 
-  const backgroundColor = cssObj.getPropertyValue('background-color'); 
-  event.target.style.backgroundColor = backgroundColor;   
+function changeColor(event) {
+  const cssObj = window.getComputedStyle(currentColorSelected, null);// Referência retirada, até linha 18, do site W3 Schools <https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp> 
+  const backgroundColor = cssObj.getPropertyValue('background-color');
+  event.target.style.backgroundColor = backgroundColor;
 }
 // limpa os pixels
 function clearPixels() {
-for (let index = 0; index < pixels.length; index+=1) {
-  pixels[index].style.backgroundColor = 'white';
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
   }
-} 
+}
